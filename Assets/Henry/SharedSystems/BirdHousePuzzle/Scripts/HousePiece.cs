724883f9192217884ad4808cce05aec00afe0718
Henry-Agyemang <playersojo@gmail.com>
2025-11-24 12:28:41 +0000

    sound puzzle event win condition

--- Assets/Henry/SharedSystems/BirdHousePuzzle/Scripts/HousePiece.cs
+++ Assets/Henry/SharedSystems/BirdHousePuzzle/Scripts/HousePiece.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class HousePiece : MonoBehaviour
 {
-    public int pieceId = 0;   // set in Inspector (1,2,3...)
-    [HideInInspector] public bool isLocked = false;
+    [Header("ID")]
+    public int pieceId = 0;                  // 1 = base, 2 = pole, 3 = house etc.
+
+    [HideInInspector] public bool isLocked;  // set by controller
+
+    [Header("Disable when locked")]
+    // Drag XR Grab Interactable, GrabInteractable, Grabbable, etc. here
+    public Behaviour[] disableOnLock;
 }
