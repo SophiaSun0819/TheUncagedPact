597dabe12c79928b11649e5e5c65b8c3d4be2bab
sophia <sophia@sophiade-MacBook-Pro-2.local>
2025-12-12 10:27:32 +0000

    Try fix number detecter

--- Assets/Scripts/LevelManagers/prefabsController.cs
+++ Assets/Scripts/LevelManagers/prefabsController.cs
@@ -10,16 +10,16 @@ public class prefabsController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckYButton();
+        //CheckYButton();
     }
 
-  
+
 
     // public void OnButtonClick()
     // {
